674cd3e9b194018be2ae1f7db6c1aff5ee310bde
joshisaurabh <reachme15@yahoo.com>
2013-11-10 17:07:22 -0500

    Added user model, controller and view with user authentication using Bcrypt gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rainforest::Application.routes.draw do
+ 
   root 'products#index'
   resources :products
+  resources :users, :only => [:new, :create]
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131110152247) do
+ActiveRecord::Schema.define(version: 20131110211428) do
 
   create_table "products", force: true do |t|
     t.string   "name"
     t.text     "description"
     t.integer  "price_in_cents"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
