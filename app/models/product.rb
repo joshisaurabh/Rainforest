957dd68c92c982c3f31910fd1b77c5aa88c169d4
joshisaurabh <reachme15@yahoo.com>
2013-11-10 15:46:28 -0500

    product validations added

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ActiveRecord::Base
+   validates :name, presence: true, length: {minimum: 3}
+   	validates :description, presence: true, length: {minimum: 3}
+   	validates :price_in_cents, presence: true
 end
