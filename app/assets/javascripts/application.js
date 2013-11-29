bcfc6621fc0247f2422d4f9ff9fa5b4929004325
joshisaurabh <reachme15@yahoo.com>
2013-11-28 21:44:07 -0500

    infinit scroll feature view working properly now

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,34 +16,34 @@
 //= require_tree .
 
 
- jQuery(function() {
-   $(window).scroll(function() {
-     if ($(window).scrollTop() > $(document).height() - $(window).height() - 50) {
-       return alert('near bottom');
-     }
-   });
- });
+ // jQuery(function() {
+ //   $(window).scroll(function() {
+ //     if ($(window).scrollTop() > $(document).height() - $(window).height() - 50) {
+ //       return alert('near bottom');
+ //     }
+ //   });
+ // });
 
 
 jQuery(function() {
   $(window).scroll(function() {
     if ($(window).scrollTop() > $(document).height() - $(window).height() - 50) {
-      console.log($('.pagination span.next').eq(-2).children().attr('href'));
-      $.getScript($('.pagination span.next').eq(-2).children().attr('href'));
+      console.log($('.pagination span.next').children().attr('href'));
+      $.getScript($('.pagination span.next').children().attr('href'));
     }
   });
 });
 
 
-jQuery(function() {
-  if ($('.pagination').length) {
-    $(window).scroll(function() {
-      var url;
-      url = $('.pagination span.next').eq(-2).children().attr('href');
-      if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
-        $('.pagination').text("Fetching more products...");
-        return $.getScript(url);
-      }
-    });
-  }
-})
+// jQuery(function() {
+//   if ($('.pagination').length) {
+//     $(window).scroll(function() {
+//       var url;
+//       url = $('.pagination span.next').eq(-2).children().attr('href');
+//       if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
+//         $('.pagination').text("Fetching more products...");
+//         return $.getScript(url);
+//       }
+//     });
+//   }
+// })
